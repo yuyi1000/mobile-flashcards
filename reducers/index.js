e9fdd528b5bb5aa17740f8a4178edcb148637293
Yi <yy471014@ohio.edu>
2018-08-31 17:09:34 -0400

    add a reducer to deal with action ADD_NEW_QUESTION

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS } from '../actions'
+import { RECEIVE_DECKS, ADD_NEW_QUESTION } from '../actions'
 
 export default function decks (state={}, action) {
   switch (action.type) {
@@ -6,6 +6,18 @@ export default function decks (state={}, action) {
       return {
         ...state,
         ...action.decks,
+      }
+    case ADD_NEW_QUESTION :
+      const { title, question, answer } = action
+      return {
+        ...state,
+        [title]: {
+          ...state[title],
+          questions: {
+            ...state[title].questions,
+            [question]: [answer]
+          }
+        }
       }
     default :
       return state
