18b3aa3d76f3ba32631f6c5aaa3c786609b50c28
Yi <yy471014@ohio.edu>
2018-08-31 10:27:44 -0400

    add deck title and number of cards in EachDeck View

--- components/EachDeck.js
+++ components/EachDeck.js
@@ -5,23 +5,40 @@ import { getDeck } from '../utils/api'
 class EachDeck extends Component {
 
   state = {
-    deck: {}
+    deck: {},
+    ready: false,
   }
 
   componentDidMount() {
     const deckTitle = this.props.navigation.getParam('deckTitle', 'NO-TITLE')
     getDeck(deckTitle)
-      .then((deck) => this.setState({deck}))
+      .then((deck) => this.setState({deck, ready: true}))
   }
 
   render() {
-    const { deck } = this.state
-    console.log(deck)
+    const { deck, ready } = this.state
+
+    if (!ready) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>
+            Still loading data.
+          </Text>
+        </View>
+      )
+    }
+
+
+    console.log(deck);
+    const title = deck.title
+    const numberOfCards = deck.questions.length
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>
           This is EachDeck View.
         </Text>
+        <Text>{title}</Text>
+        <Text>{numberOfCards}</Text>
       </View>
     )
   }
