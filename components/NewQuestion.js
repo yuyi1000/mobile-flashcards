cbd3f635d4aa8ed56f98422dfc6875ed18ee6bc1
Yi <yy471014@ohio.edu>
2018-08-31 11:16:04 -0400

    add a textinput field for a new question in NewQuestion View

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 
 class NewQuestion extends Component {
+  state = {
+    question: '',
+    answer: '',
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>
           This is NewQuestion View.
         </Text>
+        <TextInput
+          placeholder='Question'
+          onChangeText={(text) => this.setState({question: text})}
+        />
       </View>
     )
   }
