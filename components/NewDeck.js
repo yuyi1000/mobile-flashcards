0de46d726e6f50fc8ade27e3b94741e76491aff0
Yi <yy471014@ohio.edu>
2018-09-01 10:43:18 -0400

    add textinput field for deck title in NewDeck View

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TextInput } from 'react-native'
 
 class NewDeck extends Component {
+  state = {
+    title: '',
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -11,7 +15,10 @@ class NewDeck extends Component {
         <Text>
           What is the title of your new deck?
         </Text>
-
+        <TextInput style={{ margin: 10, borderWidth: 1 }}
+          placeholder='Deck Title'
+          onChangeText={(text) => this.setState({title: text})}
+        />
 
       </View>
 
