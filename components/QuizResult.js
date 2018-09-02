79f107c89b3357abec95825849e1bdd4c03cb45f
Yi <yy471014@ohio.edu>
2018-09-02 16:45:33 -0400

    add skeleton QuizResult View

---
+++ components/QuizResult.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class QuizResult extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>
+          This is QuizResult View.
+        </Text>
+      </View>
+    )
+  }
+
+}
+
+export default QuizResult
