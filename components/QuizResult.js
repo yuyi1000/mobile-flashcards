da7a8d77993e955f0f405b0b61d69054bcfa55f7
Yi <yy471014@ohio.edu>
2018-09-03 09:58:28 -0400

    show number of corrected and total answer in QuizResult View

--- components/QuizResult.js
+++ components/QuizResult.js
@@ -3,10 +3,16 @@ import { View, Text } from 'react-native'
 
 class QuizResult extends Component {
   render() {
+
+    const { correctedQuestions, answeredQuestions } = this.props.navigation.state.params
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>
           This is QuizResult View.
+        </Text>
+        <Text>
+          You got {correctedQuestions} out of {answeredQuestions} questions answered correctly.
         </Text>
       </View>
     )
