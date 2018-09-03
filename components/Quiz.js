0f962fead3d3dba4e68a0feaddfa9de44147bf11
Yi <yy471014@ohio.edu>
2018-09-02 20:50:57 -0400

    add ready state to check if component is ready for render in Quiz View

--- components/Quiz.js
+++ components/Quiz.js
@@ -6,16 +6,50 @@ import { connect } from 'react-redux'
 class Quiz extends Component {
 
   state = {
+    ready: false,
     answeredQuestions: 0,
+    showQuestion: true,
     deck: {}
   }
 
   componentDidMount() {
     const deck = this.props.navigation.getParam('deck', {})
-    this.setState({deck})
+    console.log(deck);
+    this.setState({
+      deck,
+      ready: true,
+    })
   }
 
   render() {
+    const { showQuestion, deck, answeredQuestions, ready } = this.state
+    if (!ready) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>
+            Still loading.
+          </Text>
+        </View>
+      )
+    }
+    // console.log(deck);
+    // console.log(this.state);
+    if (showQuestion) {
+      const questionText = deck.questions[answeredQuestions].question
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>
+            This is Quiz View.
+          </Text>
+          <Text>
+            {questionText}
+          </Text>
+
+
+        </View>
+      )
+    }
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>
