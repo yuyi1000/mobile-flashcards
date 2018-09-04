df0284ac289e4d455a4966dc245f8335d584a6c8
Yi <yy471014@ohio.edu>
2018-09-04 12:08:15 -0400

    reuse TextButton in QuizResult View

--- components/QuizResult.js
+++ components/QuizResult.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import TextButton from './TextButton'
 
 class QuizResult extends Component {
   render() {
@@ -12,58 +13,13 @@ class QuizResult extends Component {
           You got {correctedQuestions} out of {answeredQuestions} questions answered correctly.
         </Text>
 
-        <TouchableOpacity
-          style={{
-            width: 200,
-            marginRight:40,
-            marginLeft:40,
-            marginTop:10,
-            paddingTop:10,
-            paddingBottom:10,
-            backgroundColor:'#1E6738',
-            borderRadius:10,
-            borderWidth: 1,
-            borderColor: '#fff'
-          }}
-          onPress={() => this.props.navigation.goBack()}
-          >
-          <Text style={{
-            color:'#fff',
-            textAlign:'center',
-            paddingLeft : 10,
-            paddingRight : 10
-          }}
-          >
-            Start again
-          </Text>
-        </TouchableOpacity>
+        <TextButton onPress={() => this.props.navigation.goBack()}>
+          Start again
+        </TextButton>
 
-
-        <TouchableOpacity
-          style={{
-            width: 200,
-            marginRight:40,
-            marginLeft:40,
-            marginTop:10,
-            paddingTop:10,
-            paddingBottom:10,
-            backgroundColor:'#1E6738',
-            borderRadius:10,
-            borderWidth: 1,
-            borderColor: '#fff'
-          }}
-          onPress={() => this.props.navigation.navigate('DeckList')}
-          >
-          <Text style={{
-            color:'#fff',
-            textAlign:'center',
-            paddingLeft : 10,
-            paddingRight : 10
-          }}
-          >
-            Check another deck
-          </Text>
-        </TouchableOpacity>
+        <TextButton onPress={() => this.props.navigation.navigate('DeckList')}>
+          Check another deck
+        </TextButton>
 
       </View>
     )
