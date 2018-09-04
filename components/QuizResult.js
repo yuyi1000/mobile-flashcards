a191814b36e49143bb1507afa90b3893c003263b
Yi <yy471014@ohio.edu>
2018-09-04 10:38:24 -0400

    add style for buttons in QuizResult View

--- components/QuizResult.js
+++ components/QuizResult.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TouchableOpacity } from 'react-native'
 
 class QuizResult extends Component {
   render() {
@@ -8,17 +8,65 @@ class QuizResult extends Component {
 
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>
+        <Text style={{ fontSize: 20, padding: 20, }}>
           You got {correctedQuestions} out of {answeredQuestions} questions answered correctly.
         </Text>
-        <Button
-          title='Start again'
+
+        <TouchableOpacity
+          style={{
+            width: 200,
+            marginRight:40,
+            marginLeft:40,
+            marginTop:10,
+            paddingTop:10,
+            paddingBottom:10,
+            backgroundColor:'#1E6738',
+            borderRadius:10,
+            borderWidth: 1,
+            borderColor: '#fff'
+          }}
           onPress={() => this.props.navigation.goBack()}
-        />
-        <Button
-          title='Check another deck'
+          >
+          <Text style={{
+            color:'#fff',
+            textAlign:'center',
+            paddingLeft : 10,
+            paddingRight : 10
+          }}
+          >
+            Start again
+          </Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+          style={{
+            width: 200,
+            marginRight:40,
+            marginLeft:40,
+            marginTop:10,
+            paddingTop:10,
+            paddingBottom:10,
+            backgroundColor:'#1E6738',
+            borderRadius:10,
+            borderWidth: 1,
+            borderColor: '#fff'
+          }}
           onPress={() => this.props.navigation.navigate('DeckList')}
-        />
+          >
+          <Text style={{
+            color:'#fff',
+            textAlign:'center',
+            paddingLeft : 10,
+            paddingRight : 10
+          }}
+          >
+            Check another deck
+          </Text>
+        </TouchableOpacity>
+
+
+
       </View>
 
     )
